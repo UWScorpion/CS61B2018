a85a739fe9fee302d040c31982393ddd291295ec
Ninja AZ <lovepinklol123@gmail.com>
2020-05-28 19:29:01 -0700

    Add files via upload

---
+++ lab9/lab9/MyHashMap.java
@@ -0,0 +1,125 @@
+package lab9;
+
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ *  A hash table-backed Map implementation. Provides amortized constant time
+ *  access to elements via get(), remove(), and put() in the best case.
+ *
+ *  @author Alex
+ */
+public class MyHashMap<K, V> implements Map61B<K, V> {
+
+    private static final int DEFAULT_SIZE = 16;
+    private static final double MAX_LF = 0.75;
+
+    private ArrayMap<K, V>[] buckets;
+    private int size;
+
+    private double loadFactor() {
+        return (double) size / buckets.length;
+    }
+
+    public MyHashMap() {
+        buckets = new ArrayMap[DEFAULT_SIZE];
+        this.clear();
+    }
+
+    /* Removes all of the mappings from this map. */
+    @Override
+    public void clear() {
+        this.size = 0;
+        for (int i = 0; i < this.buckets.length; i += 1) {
+            this.buckets[i] = new ArrayMap<>();
+        }
+    }
+
+    /** Computes the hash function of the given key. Consists of
+     *  computing the hashcode, followed by modding by the number of buckets.
+     *  To handle negative numbers properly, uses floorMod instead of %.
+     */
+    private int hash(K key) {
+        if (key == null) {
+            return 0;
+        }
+
+        int numBuckets = buckets.length;
+        return Math.floorMod(key.hashCode(), numBuckets);
+    }
+
+    /* Returns the value to which the specified key is mapped, or null if this
+     * map contains no mapping for the key.
+     */
+    @Override
+    public V get(K key) {
+        return buckets[hash(key)].get(key);
+    }
+
+    /* Associates the specified value with the specified key in this map. */
+    @Override
+    public void put(K key, V value) {
+        if (!buckets[hash(key)].containsKey(key)) {
+            size++;
+        }
+        buckets[hash(key)].put(key, value);
+        if (loadFactor() >= MAX_LF){
+            //resize the buckets when the loadFactor is larger than 0.75
+            resize(buckets.length * 2);
+
+        }
+    }
+    private void resize(int newLen){
+        ArrayMap<K, V>[] newbuckets = new ArrayMap[newLen];
+        for (int i = 0; i < newLen; i++){
+            newbuckets[i] = new ArrayMap<>();
+        }
+        ArrayMap<K, V>[] oldbuckets = buckets;
+        buckets = newbuckets;
+        // copy (key, value) from old buckets to the new buckets
+        for (int i = 0; i < oldbuckets.length; i++){
+            if (oldbuckets[i].size != 0){
+                for (K key:oldbuckets[i].keySet()){
+                    if(key != null && oldbuckets[i].get(key) != null) {
+                        buckets[hash(key)].put(key, oldbuckets[i].get(key));
+                    }
+                }
+            }
+        }
+    }
+
+    /* Returns the number of key-value mappings in this map. */
+    @Override
+    public int size() {
+        return size;
+    }
+
+    //////////////// EVERYTHING BELOW THIS LINE IS OPTIONAL ////////////////
+
+    /* Returns a Set view of the keys contained in this map. */
+    @Override
+    public Set<K> keySet() {
+        throw new UnsupportedOperationException();
+    }
+
+    /* Removes the mapping for the specified key from this map if exists.
+     * Not required for this lab. If you don't implement this, throw an
+     * UnsupportedOperationException. */
+    @Override
+    public V remove(K key) {
+        throw new UnsupportedOperationException();
+    }
+
+    /* Removes the entry for the specified key only if it is currently mapped to
+     * the specified value. Not required for this lab. If you don't implement this,
+     * throw an UnsupportedOperationException.*/
+    @Override
+    public V remove(K key, V value) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Iterator<K> iterator() {
+        throw new UnsupportedOperationException();
+    }
+}
